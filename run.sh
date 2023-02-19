a3e419314086cc0c3ea32bbff33993320315fbe0
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-19 14:42:29 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=TELSVirus
+#SBATCH --job-name=TLS-virl
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=<email>
 #SBATCH --ntasks=1
@@ -15,4 +15,4 @@ snakemake --cluster "sbatch -A {cluster.account} -q {cluster.qos} \
 --mem {cluster.mem} -J {cluster.jobname} --mail-type={cluster.mail_type} \
 --mail-user={cluster.mail} --output {cluster.out} --error {cluster.err}" \
 --cluster-config cluster.json --jobs 100 --latency-wait 20 \
---rerun-incomplete --use-envmodules
+--rerun-incomplete --use-envmodules --use-conda
