c21f1abca8f2e247f7265193749fd7caf368df40
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-19 14:44:45 -0500

    Create find_viral_targets.py
    This script takes creates a bed file for subsetting the `all_viruses` fasta

---
+++ scripts/find_viral_targets.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mpileup', required = True)
+    parser.add_argument('--bed', required = False)
+    parser.add_argument('--outfile', required = False)
+    return parser.parse_args()
+
+def get_counts(pileup):
+    covered_counts = {}
+    for row in pileup:
+        try: covered_counts[row[0]] += 1
+        except KeyError: covered_counts[row[0]] = 1
+    return covered_counts
+
+def write_viral_targets(total_counts, covered_counts, outfile):
+    with open(outfile, "w") as o:
+        for key in covered_counts.keys():
+            percent = (covered_counts[key]/total_counts[key]) * 100
+            if percent > 80.0:
+                o.write(f'{key}\t0\t{total_counts[key]}\n')
+
+def main():
+    args = parse_args()
+    pileup = (row.split('\t') for row in open(args.mpileup))
+    bed = (row.split('\t') for row in open(args.bed))
+    total_counts = dict([(row[0], row[2]) for row in bed])
+    covered_counts = get_counts(pileup)
+    write_viral_targets(total_counts, covered_counts, args.outfile)
+
+if __name__ == '__main__':
+    main()
