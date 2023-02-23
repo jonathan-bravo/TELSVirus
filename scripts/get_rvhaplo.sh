69530e9074f24ea5588b83d1d8eb254ea79ee8d6
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-23 15:13:44 -0500

    Update get_rvhaplo.sh
    updated calls so they are correct for the pipeline

--- scripts/get_rvhaplo.sh
+++ scripts/get_rvhaplo.sh
@@ -1,3 +1,6 @@
 git clone $1;
 chmod +x RVHaplo/rvhaplo.sh;
+chmod +x RVHaplo/src/*;
+sed -i 's^./src/^scripts/RVHaplo/src/^' RVHaplo/rvhaplo.sh;
+sed -i 's^./src/^scripts/RVHaplo/src/^' RVHaplo/src/out_haplotypes.py;
 mv RVHaplo/ scripts/
