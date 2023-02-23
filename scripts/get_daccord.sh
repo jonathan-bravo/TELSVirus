11a30cc260070af8b0c73a237ff4fcb0f48b8627
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-23 15:13:56 -0500

    Update get_daccord.sh

--- scripts/get_daccord.sh
+++ scripts/get_daccord.sh
@@ -2,4 +2,5 @@ wget https://github.com/gt1/daccord/releases/download/0.0.10-release-20170526170
 tar -zvxf daccord-0.0.10-release-20170526170720-x86_64-etch-linux-gnu.tar.gz;
 rm daccord-0.0.10-release-20170526170720-x86_64-etch-linux-gnu.tar.gz;
 mv daccord-0.0.10-release-20170526170720-x86_64-etch-linux-gnu/ scripts/;
-mv scripts/daccord-0.0.10-release-20170526170720-x86_64-etch-linux-gnu/ scripts/daccord/
+mv scripts/daccord-0.0.10-release-20170526170720-x86_64-etch-linux-gnu/ scripts/daccord/;
+chmod +x scripts/daccord/bin/daccord
