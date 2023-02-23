fa457059f9c6267ae0c52e378efbf4f6ea8a0ee1
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-23 15:14:22 -0500

    Update find_viral_targets.py
    Will be further updating as listed in README

--- scripts/find_viral_targets.py
+++ scripts/find_viral_targets.py
@@ -2,6 +2,9 @@
 
 import argparse
 
+## WHAT THE SUBSTRAIN THAT HAS HIGHEST COVERAGE
+## INCLUDE DEPTH OF COVERAGE TO BREAK TIES
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mpileup', required = True)
@@ -12,16 +15,17 @@ def parse_args():
 def get_counts(pileup):
     covered_counts = {}
     for row in pileup:
-        try: covered_counts[row[0]] += 1
-        except KeyError: covered_counts[row[0]] = 1
+        if int(row[1]) > 0:
+            try: covered_counts[row[0]] += 1
+            except KeyError: covered_counts[row[0]] = 1
     return covered_counts
 
 def write_viral_targets(total_counts, covered_counts, outfile):
     with open(outfile, "w") as o:
         for key in covered_counts.keys():
-            percent = (covered_counts[key]/total_counts[key]) * 100
+            percent = (covered_counts[key]/int(total_counts[key])) * 100
             if percent > 80.0:
-                o.write(f'{key}\t0\t{total_counts[key]}\n')
+                o.write(f'{key}\t0\t{total_counts[key]}')
 
 def main():
     args = parse_args()
