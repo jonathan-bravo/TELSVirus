960d94aaa4c7eaa28694322203112e73faeea842
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-23 19:53:02 -0500

    Update samtools_idxstats.py
    made script more memory efficient (and shorter)

--- scripts/samtools_idxstats.py
+++ scripts/samtools_idxstats.py
@@ -1,42 +1,29 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 
 def parse_cmdline_params():
-    info = "Parses a Samtools idxstats file to obtain the total number of mapped, unmapped, and total reads"
-    parser = argparse.ArgumentParser(description=info)
-    parser.add_argument(
-        '-i', '--input_files', nargs='+', required=True,
-        help='Use globstar to pass a list of files, (Ex: *.tsv)')
-    parser.add_argument(
-        '-o', '--output_file', required=True,
-        help='Output file to write mapping results to')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', nargs='+', required=True)
+    parser.add_argument('-o', required=True)
     return parser.parse_args()
 
-def header(output_file):
-    with open(output_file, 'a') as o:
+def write_out(idxstats, output_file):
+    with open(output_file, 'w') as o:
         o.write('Sample\tNumberOfInputReads\tMapped\tUnmapped\n')
+        [o.write(stat) for stat in idxstats]
 
-def mapping_stats(input_list, output_file):
-    for f in input_list:
-        mapped = 0
-        unmapped = 0
-        number_of_reads = 0
-        with open(f, 'r') as fp:
-            sample_name = os.path.basename(str(fp.name)).split('.', 1)[0]
-            for line in fp:
-                columns = line.strip().split('\t')
-                mapped += int(columns[2])
-                unmapped += int(columns[3])
-            number_of_reads += mapped + unmapped
-        with open(output_file, 'a') as o:
-            o.write(f'{sample_name}\t{number_of_reads}\t{mapped}\t{unmapped}\n')
+def mapping_stats(infile):
+    sample_name = infile.split('/')[-1].split('.', 1)[0]
+    idxstats = ((int(row.strip().split('\t')[2]),int(row.strip().split('\t')[3])) for row in open(infile))
+    mapped, unmapped = [sum(x) for x in zip(*idxstats)]
+    number_of_reads = mapped + unmapped
+    return f'{sample_name}\t{number_of_reads}\t{mapped}\t{unmapped}\n'
 
 def main():
-    opts = parse_cmdline_params()
-    header(opts.output_file)
-    mapping_stats(opts.input_files, opts.output_file)
+    args = parse_cmdline_params()
+    idxstats = list(map(mapping_stats, args.i))
+    write_out(idxstats, args.o)
 
 if __name__ == "__main__":
     main()
