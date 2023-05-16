5834a2d3466aa925c4ffd8b96c267d6158af0358
Johnny Bravo <hakmonkey@gmail.com>
2023-05-16 04:23:44 -0400

    Update trim.py
    I think I am on the final version of this script. Will get final approval, may update how `read_info` is logged, but other than that it appears to be working as desired.

--- trim_test/trim.py
+++ trim_test/trim.py
@@ -3,123 +3,208 @@
 import argparse
 import gzip
 from datetime import date
-from Bio import SeqIO
+from Bio import SeqIO, bgzf
 import re
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--infile', required=True)
-    parser.add_argument('--crop', type=int)
-    parser.add_argument('--logfile')
-    parser.add_argument('--outfile')
-    parser.add_argument('--adapters', nargs='+')
+    parser.add_argument('--crop', type=int, required=True)
+    parser.add_argument('--logfile', required=True)
+    parser.add_argument('--outfile', required=True)
+    parser.add_argument('--barcodes', required=True)
     return parser.parse_args()
+
 
 def get_reads(fq):
     return (seq for seq in SeqIO.parse(gzip.open(fq, 'rt'), 'fastq'))
 
-def get_adapters(fq):
+
+def get_barcodes(fq):
     return [str(fasta.seq) for fasta in SeqIO.parse(open(fq), 'fasta')]
 
-def make_log(log, crop):
-    with open(log, 'w') as o:
+
+def make_log(crop): # change this to reflect changes to code
+    with open(logfile, 'w') as o:
         o.write(f'{date.today()}\n\n')
-        o.write(f'Cropping {crop} bases from head and tail of all reads\n\n')
+        o.write(f'Base crop length is {crop} bases from head and tail of all reads\n')
+        o.write('Some reads may have this value adjusted, this will be listed per read\n\n')
 
-def make_fq(fq):
-    open(fq, 'wb').close()
 
-def write_seq_log(logfile, read_tag, seq):
+def make_fq():
+    open(outfile, 'wb').close()
+
+
+def write_seq_log(read, head_crop, tail_crop):
     with open(logfile, 'a') as o:
-        o.write(f'>{read_tag}')
-        o.write(f'{seq}\n')
+        o.write('sequence too short with crop lengths:\n')
+        o.write(f'head crop len: {head_crop}, tail crop len: {-tail_crop}\n')
+        o.write(f'>{read.id}\n')
+        o.write(f'{str(read.seq)}\n\n')
 
-def write_crop_log(logfile, read_tag, seq, crop):
+
+def write_crop_log(read, head_crop, tail_crop):
     with open(logfile, 'a') as o:
-        o.write(f'>{read_tag}_head\n')
-        o.write(f'{seq[:crop]}\n')
-        o.write(f'>{read_tag}_tail\n')
-        o.write(f'{seq[-crop:]}\n\n')
-       
-def write_read(outfile, read_id, seq, desc, qual, crop):
-    with gzip.open(outfile, 'a') as o:
-        o.write(read_id)
-        o.write(seq[crop:-crop] + b'\n')
-        o.write(desc)
-        o.write(qual[crop:-crop] + b'\n')
-
-# def process_read(read, reads, crop, outfile, logfile):
-#     read_id = read
-#     seq = next(reads)
-#     seq_len = len(seq.strip())
-#     desc = next(reads)
-#     qual = next(reads)
-#     read_tag = read_id.split(b' ')[0][1:].decode('utf-8')
-#     if seq_len >= (2*crop)+1:
-#         write_read(outfile, read_id, seq.strip(), desc, qual.strip(), crop)
-#         write_crop_log(logfile, read_tag, seq.strip().decode('utf-8'), crop)
-#     else:
-#         write_seq_log(logfile, read_tag, seq.decode('utf-8'))
-
-def in_head(seq, span):
-    return span[1] <= len(seq)//2
-
-def in_tail(seq, span):
-    return span[0] >= len(seq)//2
-
-def centered(seq, span):
-    return span[0] < len(seq)//2 and span[1] > len(seq)//2
-
-def process_read(read, adapters, barcodes, crop, outfile, logfile):
-        # if any(barcode in read for barcode in barcodes):
-        #     print('yes')
-        head_crop = crop
-        tail_crop = crop
-        not_found = True
-        seq = str(read.seq)
-        for barcode in barcodes:
-            if barcode in seq:
-                not_found = False
-                span = re.search(rf'{barcode}', seq).span()
-                all_match = re.findall(rf'{barcode}', seq)
-                matches = []
-                for match in re.finditer(rf'{barcode}', seq):
-                    matches.append((match.start(), match.end(), match.group()))
-                print(read.id, matches)
-        if not_found:
-            print(read.id, 'NONE')
-        #         print(all_match)
-        #         if centered(seq, span):
-        #             print('center')
-        #             #print(f'in {read.id} len {len(seq)} center {span}')
-        #         else:
-        #             if in_head(seq, span):
-        #                 #head_crop = pos + 24
-        #                 print('head')
-        #                 #print(f'in {read.id} len {len(seq)} head {pos}')
-        #             if in_tail(seq, span):
-        #                 print('tail')
-        #                 #print(f'in {read.id} len {len(seq)} tail {pos}')
-        # # if not_found:
-        # #     print(f'just crop {read.id}')
+        o.write(f'>{read.id}\n')
+        o.write(f'head crop len: {head_crop}, tail crop len: {-tail_crop}\n')
+        o.write(f'cropped head seq\n')
+        o.write(f'{str(read.seq)[:head_crop]}\n')
+        o.write(f'cropped tail seq\n')
+        o.write(f'{str(read.seq)[tail_crop:]}\n\n')
 
 
+def write_read(read, head_crop, tail_crop):
+    with bgzf.BgzfWriter(outfile, 'ab') as o:
+        SeqIO.write(read[head_crop:tail_crop], o, 'fastq')
 
-# def trim_reads(reads, adapters, barcode, crop, outfile, logfile):
-#     [process_read(read, reads, crop, outfile, logfile) for read in reads]
 
-def trim_reads(reads, adapters, barcodes, crop, outfile, logfile):
-    [process_read(read, adapters, barcodes, crop, outfile, logfile) for read in reads]
+def write_read_info():
+    with open(logfile, 'a') as o:
+        o.write('ReadID\tReadLength\tNumMatches\t(Head,Tail,Neither)\n')
+        [o.write(f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\n') for i in read_info]
+
+
+def long_enough(seq, head_crop, tail_crop): # - tail_crop because it is negative
+    return len(seq) >= (head_crop - tail_crop + 50)
+
+
+def in_head(barcode):
+    return 0 <= barcode[0] <= 50
+
+
+def in_tail(barcode, seq_len):
+    return seq_len - 50 <= barcode[0] <= seq_len
+
+
+def find_matches(barcode, seq):
+    # removed match.group() from tuple because it is the sequence of the barcode
+    return [(m.start(), m.end()) for m in re.finditer(rf'{barcode}', seq)]
+
+
+def barcode_check(seq):
+    return [b for bar in barcodes for b in find_matches(bar, seq) if bar in seq]
+
+
+def trim_read(read, head_crop, tail_crop):
+    write_read(read, head_crop, tail_crop)
+    write_crop_log(read, head_crop, tail_crop)
+
+
+def point_read(read, head_crop, tail_crop):
+    if long_enough(str(read.seq), head_crop, tail_crop):
+        trim_read(read, head_crop, tail_crop)
+    else:
+        write_seq_log(read, head_crop, tail_crop)
+
+
+def split_read(read, barcode):
+    read_a = read[:barcode[0]]
+    read_b = read[barcode[0]:]
+    read_a.read_id = f'{read_a.id}_A'
+    read_b.read_id = f'{read_b.id}_B'
+    process_read(read_a)
+    process_read(read_b)
+
+
+def classify_match(match, seq_len):
+    if in_head(match): return 'head'
+    if in_tail(match, seq_len): return 'tail'
+    return 'neither'
+
+
+def bin_barcodes(matches, seq_len): # if this doesn't work I can use the below code
+
+    bins = {'head': [], 'tail': [], 'neither': []}
+
+    [bins[classify_match(m, seq_len)].append(m) for m in matches]
+
+    head = bins['head']
+    tail = bins['tail']
+    neither = bins['neither']
+
+    head.sort()
+    tail.sort()
+    neither.sort()
+
+    return (head, tail, neither)
+
+# def bin_barcodes(matches, seq_len):
+#     neither, head, tail = [], [], []
+
+#     bins = {'head': [], 'tail': [], 'neither': []}
+
+#     for match in matches:
+#         if in_head(match):
+#             head.append(match)
+#         elif in_tail(match, seq_len):
+#             tail.append(match)
+#         else:
+#             neither.append(match)
+
+#     head.sort()
+#     tail.sort()
+#     neither.sort()
+
+#     return (head, tail, neither)
+
+
+def process_read(read):
+    head_crop = crop_len
+    tail_crop = -crop_len
+    
+    matches = barcode_check(str(read.seq))
+
+    head, tail, neither = bin_barcodes(matches, len(str(read.seq)))
+
+    read_info.append((
+        read.id,
+        len(str(read.seq)),
+        len(matches),
+        (len(head), len(tail), len(neither))
+    ))
+
+    if len(neither) > 0:
+        barcode = neither[0]
+        split_read(read, barcode)
+    else:
+        if len(head) > 0: head_crop = head[-1][1] + 13
+        if len(tail) > 0: tail_crop = tail[0][0] - 13 - len(str(read.seq))
+        point_read(read, head_crop, tail_crop)
+
 
 def main():
+    # declaring globals
+    global barcodes
+    global crop_len
+    global logfile
+    global outfile
+    global read_info
+
+    # parsing input arguments
     args = parse_args()
+
+    # getting all reads as generator object
     reads = get_reads(args.infile)
-    illumina_adapters = get_adapters(args.adapters[0])
-    barcodes = get_adapters(args.adapters[1])
-    #for seq in reads: print(seq)
-    make_log(args.logfile, args.crop)
-    make_fq(args.outfile)
-    trim_reads(reads, illumina_adapters, barcodes, args.crop, args.outfile, args.logfile)
+
+    # setting values for global variables
+    barcodes = get_barcodes(args.barcodes)
+    crop_len = args.crop
+    logfile = args.logfile
+    outfile = args.outfile
+    read_info = []
+
+    # make the inistial log and out file
+    make_log(args.crop)
+    make_fq()
+
+    # begin processing
+    [process_read(read) for read in reads]
+
+    write_read_info()
+
 
 if __name__ == '__main__':
     main()
+
+# ./trim.py --infile barcode09.fastq.gz --outfile barcode09.trim.fastq --crop 37 --logfile barcode09.trim.log --barcodes RapidBarcode.fasta
