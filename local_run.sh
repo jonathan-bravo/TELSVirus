be08211a21d20cd828ef2fbc2b9d883163d26f61
Johnny Bravo <HaKmonkey@gmail.com>
2023-08-01 21:34:34 -0400

    Split pipeline
    Split the pipeline into two different workflows because some samples did not have any good viral targets and I could not find a way to branch the pipeline based on sample output the way the pipeline was previously structured.

---
+++ local_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+threads=$1
+
+snakemake -s TELSVirus_p1 -c ${threads} \
+--use-conda --rerun-incomplete --latency-wait 20 --forceall
+
+snakemake -s TELSVirus_p2 -c ${threads} \
+--use-conda --rerun-incomplete --latency-wait 20 --forceall
