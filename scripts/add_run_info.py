0d32c50304c055a85d7f34e7b43b4fe6cb0b7984
HaKmonkey <hakmonkey@proton.me>
2023-10-12 15:46:24 -0400

    Update add_run_info.py

--- scripts/add_run_info.py
+++ scripts/add_run_info.py
@@ -5,22 +5,24 @@ from argparse import ArgumentParser
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('--sample_id')
-    parser.add_argument('--run_id')
-    parser.add_argument('--infiles', nargs='+')
+    parser.add_argument('--sample_id', required=True)
+    parser.add_argument('--run_id', required=True)
+    parser.add_argument('--infiles', nargs='+', required=True)
     return parser.parse_args()
 
 
+def rewrite_file(infile, run_id, sample_id):
+    original = [line for line in open(infile,'r')]
+    with open(infile, 'w') as final:
+        final.write(f'run_id\tsample_id\t{original[0]}')
+        for l in original[1:]:
+            final.write(f'{run_id}\t{sample_id}\t{l}')
+
+
 def main():
-    sample_id = 'barcode01'
-    run_id = 'June_07_2023_SHIC_Plate1_3'
     args = parse_args()
     for f in args.infiles:
-        original = [line for line in open(f,'r')]
-        with open(f, 'w') as final:
-            final.write(f'run_id\tsample_id\t{original[0]}')
-            for l in original[1:]:
-                final.write(f'{run_id}\t{sample_id}\t{l}')
+        rewrite_file(f, args.run_id, args.sample_id)
 
 
 if __name__ == '__main__':
