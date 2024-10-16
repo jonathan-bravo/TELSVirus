c3cd32f61187826c3e42b84e42ee9f1534fa560d
Johnny Bravo <hakmonkey@gmail.com>
2024-10-16 10:43:28 -0400

    Initial formatting change
    Will continue to format directory structure to align with Snakemake recommendations.

--- local_run.sh
+++ local_run.sh
@@ -2,8 +2,4 @@
 
 threads=$1
 
-snakemake -s TELSVirus_p1 -c ${threads} \
---use-conda --rerun-incomplete --latency-wait 20
-
-snakemake -s TELSVirus_p2 -c ${threads} \
---use-conda --rerun-incomplete --latency-wait 20
+snakemake -c ${threads} --use-conda --rerun-incomplete --latency-wait 20
