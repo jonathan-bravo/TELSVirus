aa2895bcf3a422bb7252cb443d7000d5220c28c9
Johnny Bravo <hakmonkey@gmail.com>
2024-10-16 23:25:46 -0400

    Update run_find_duplicates.sh

--- workflow/scripts/run_find_duplicates.sh
+++ workflow/scripts/run_find_duplicates.sh
@@ -8,7 +8,7 @@ for f in ${psl_indir}*;
 do
     file="$(basename -- $f)";
     cluster=${file%.psl};
-    python scripts/find_duplicates.py \
+    python workflow/scripts/find_duplicates.py \
     -p $f \
     -s ${threshold} \
     -o ${outdir}${cluster}.dupes.txt;
